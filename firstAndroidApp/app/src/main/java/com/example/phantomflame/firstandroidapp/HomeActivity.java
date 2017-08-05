152a3869acf3e8948a107fd69964c46c250016b7
andred <andresjoseis@gmail.com>
2017-08-05 11:45:06 -0700

    addded button to show toast

--- firstAndroidApp/app/src/main/java/com/example/phantomflame/firstandroidapp/HomeActivity.java
+++ firstAndroidApp/app/src/main/java/com/example/phantomflame/firstandroidapp/HomeActivity.java
@@ -2,12 +2,29 @@ package com.example.phantomflame.firstandroidapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.ButtonBarLayout;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class HomeActivity extends AppCompatActivity {
+
+    Button clickMeButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        clickMeButton = (Button) findViewById(R.id.click_me_button);
+
+        clickMeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(view.getContext(),"Did it!", Toast.LENGTH_SHORT).show();
+
+            }
+        });
     }
 }
